e1e3aeae62466c49fbc4cf1d482d367e62cd005c
Admin <>
2023-10-15 08:21:00 +0700

    update

---
+++ livenessSDK.xcframework/ios-arm64/livenessSDK.framework/Headers/livenessSDK.h
@@ -0,0 +1,18 @@
+//
+//  livenessSDK.h
+//  livenessSDK
+//
+//  Created by Admin on 10/14/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for livenessSDK.
+FOUNDATION_EXPORT double livenessSDKVersionNumber;
+
+//! Project version string for livenessSDK.
+FOUNDATION_EXPORT const unsigned char livenessSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <livenessSDK/PublicHeader.h>
+
+
